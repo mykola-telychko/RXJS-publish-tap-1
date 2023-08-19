76027edefd9312ff2c94fb7bcd47f6107d791d11
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-19 12:52:44 +0300

    stop

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import { interval } from 'rxjs';
 import { publish, tap } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/multicasting/publish
+
+// EXMPL - 1 - begin
 //emit value every 1 second
-const srcInterval = interval(1000);
-//do nothing until connect() is called
+const srcInterval = interval(1000); //do nothing until connect() is called
+
 const example = publish()(
   srcInterval.pipe(
     //side effects will be executed once
@@ -11,24 +14,39 @@ const example = publish()(
   )
 );
 
-/*
-  source will not emit values until connect() is called
-  output: (after 5s)
-  "Do Something!"
-  "Subscriber One: 0"
-  "Subscriber Two: 0"
-  "Do Something!"
-  "Subscriber One: 1"
-  "Subscriber Two: 1"
-*/
-const subscribe = example.subscribe((val) =>
-  console.log(`Subscriber One: ${val}`)
-);
-const subscribeTwo = example.subscribe((val) =>
-  console.log(`Subscriber Two: ${val}`)
-);
+// const subscribe = example.subscribe((val) =>
+//   console.log(`Sbscrber One: ${val}`)
+// );
+// const subscribeTwo = example.subscribe((val) =>
+//   console.log(`Sbscrber Two: ${val}`)
+// );
 
 //call connect after 5 seconds, causing source to begin emitting items
 setTimeout(() => {
   example.connect();
 }, 5000);
+// EXMPL - 1 - end
+
+// EXMPL - 2 - begin
+//emit value every 1 second
+const srcInterval = interval(1000); //do nothing until connect() is called
+
+const example = publish()(
+  srcInterval.pipe(
+    //side effects will be executed once
+    tap((_) => console.log('Do Something!'))
+  )
+);
+
+// const subscribe = example.subscribe((val) =>
+//   console.log(`Sbscrber One: ${val}`)
+// );
+// const subscribeTwo = example.subscribe((val) =>
+//   console.log(`Sbscrber Two: ${val}`)
+// );
+
+//call connect after 5 seconds, causing source to begin emitting items
+setTimeout(() => {
+  example.connect();
+}, 5000);
+// EXMPL - 1 - end
