8441ff24c788df311715be4d4bc0003f201a62e7
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-19 13:07:16 +0300

    fin

--- index.ts
+++ index.ts
@@ -5,42 +5,26 @@ import { publish, tap } from 'rxjs/operators';
 
 // EXMPL - 1 - begin
 //emit value every 1 second
+// Example 1: Connect observable after subscribers
 const srcInterval = interval(1000); //do nothing until connect() is called
 
-const example = publish()(
+const exmpl = publish()(
   srcInterval.pipe(
     //side effects will be executed once
     tap((_) => console.log('Do Something!'))
   )
 );
 
-// const subscribe = example.subscribe((val) =>
-//   console.log(`Sbscrber One: ${val}`)
-// );
-// const subscribeTwo = example.subscribe((val) =>
-//   console.log(`Sbscrber Two: ${val}`)
-// );
+const subscribe = exmpl.subscribe((v) =>
+  console.log(`Sbscrber 1: ${v}`)
+);
+const subscribeTwo = exmpl.subscribe((v) =>
+  console.log(`Sbscrber 2: ${v}`)
+);
 
-//call connect after 5 seconds, causing source to begin emitting items
-// setTimeout(() => {
-//   example.connect();
-// }, 5000);
+call connect after 5 seconds, causing source to begin emitting items
+setTimeout(() => {
+  example.connect();
+}, 5000);
 // EXMPL - 1 - end
 
-// EXMPL - 2 - begin
-// PUBLISH->INTERVAL->PIPE->TAP
-const subscribe = publish()(
-  interval(2000).pipe(
-    //side effects will be executed once
-    tap((_) => console.log('Do Something!'))
-  )
-).subscribe((val) => console.log(`Sbscrber One: ${val}`));
-// const subscribeTwo = example.subscribe((val) =>
-//   console.log(`Sbscrber Two: ${val}`)
-// );
-
-//call connect after 5 seconds, causing source to begin emitting items
-// setTimeout(() => {
-//   example.connect();
-// }, 5000);
-// EXMPL - 1 - end
