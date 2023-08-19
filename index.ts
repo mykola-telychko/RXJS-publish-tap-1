db9dc8bb3a6d15e3c6d354cedeaddf34476379c2
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-19 12:58:33 +0300

    start2

--- index.ts
+++ index.ts
@@ -22,31 +22,25 @@ const example = publish()(
 // );
 
 //call connect after 5 seconds, causing source to begin emitting items
-setTimeout(() => {
-  example.connect();
-}, 5000);
+// setTimeout(() => {
+//   example.connect();
+// }, 5000);
 // EXMPL - 1 - end
 
 // EXMPL - 2 - begin
-//emit value every 1 second
-const srcInterval = interval(1000); //do nothing until connect() is called
-
-const example = publish()(
-  srcInterval.pipe(
+// PUBLISH->INTERVAL->PIPE->TAP
+const subscribe = publish()(
+  interval(2000).pipe(
     //side effects will be executed once
     tap((_) => console.log('Do Something!'))
   )
-);
-
-// const subscribe = example.subscribe((val) =>
-//   console.log(`Sbscrber One: ${val}`)
-// );
+).subscribe((val) => console.log(`Sbscrber One: ${val}`));
 // const subscribeTwo = example.subscribe((val) =>
 //   console.log(`Sbscrber Two: ${val}`)
 // );
 
 //call connect after 5 seconds, causing source to begin emitting items
-setTimeout(() => {
-  example.connect();
-}, 5000);
+// setTimeout(() => {
+//   example.connect();
+// }, 5000);
 // EXMPL - 1 - end
